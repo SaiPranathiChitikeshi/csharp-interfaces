61f06c699bac239042a2883322b5f3cabd3d110b
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2020-12-15 09:17:47 +0530

    Add files via upload

---
+++ InterfaceExampleTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp
+{
+    class InterfaceExampleTest
+    {
+        [TestMethod]
+        public void InterfaceTest()
+        {
+            InterfaceExample inter = new InterfaceExample();
+            Assert.AreEqual(inter.getDepartmnent(), "IT");
+            Assert.AreEqual(inter.getSalary(), 1234.90);
+        }
+    }
+}
