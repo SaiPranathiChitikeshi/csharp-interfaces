807a0f11141b460746a8c0de611245708e3dc2ea
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2020-12-22 13:19:56 +0530

    Add files via upload

---
+++ expicit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace explic
+{
+    interface Iinterface_1
+
+{
+
+    void interface1_method();
+
+}
+class Class1 : Iinterface_1
+
+{
+    public void Display()
+
+    {
+
+        Console.WriteLine("Class1 Display Method.");
+        //Call Explicit interface method
+
+        Iinterface_1 obj_1 = new Class1();
+
+        obj_1.interface1_method();
+
+    }
+
+    //Implicit interface implementation
+
+    public void interface1_method()
+    {
+        Console.WriteLine("Iinterface_1 Method Implicit interface implementation.");
+
+    }
+
+ 
+
+    //Explicit interface implementation
+
+    void Iinterface_1.interface1_method()
+    {
+
+        Console.WriteLine("Iinterface_1 Method Explicit interface implementation.");
+
+    }
+
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 objClass1 = new Class1();
+
+        objClass1.Display(); //Call Implicit interface method
+        objClass1.interface1_method();
+        Console.ReadLine();
+        }
+    }
+}
---
+++ transaction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace transaction
+{
+    public interface Itransaction{
+        public void showTransaction();
+        public double getamnt();
+
+    }
+    public class Transaction:Itransaction{
+        private string Code;
+        private string date;
+        private double amnt;
+        public Transaction()
+        {
+            Code = " ";
+            date = " ";
+            amnt = 0.0;
+        }
+        public Transaction(string c, string d, double a)
+        {
+            Code = c;
+            date = d;
+            amnt = a;
+        }
+        public double getamnt()
+        {
+            return amnt;
+        }
+        public void showTransaction()
+        {
+            Console.WriteLine("Transaction: {0}", Code);
+            Console.WriteLine("Date: {0}", date);
+            Console.WriteLine("amnt: {0}", getamnt());
+ 
+    }
+    }
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Transaction t1 = new Transaction("001", "24/08/20120", 87900.00);
+            Transaction t2 = new Transaction("002", "25/10/2020", 51900.00);
+            t1.showTransaction();
+            t2.showTransaction();
+            //Console.ReadKey();
+        }
+    }
+}
+
